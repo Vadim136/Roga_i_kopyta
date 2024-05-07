b59ac45a512b3354eb5593b9186a177740a20b85
Vadim136 <barkal.bk@yandex.ru>
2024-05-07 15:04:52 +0300

    add to cartview and refactor included tempates

--- store/goods/models.py
+++ store/goods/models.py
@@ -1,38 +1,49 @@
 from django.db import models
 
+
 # Create your models here.
 class Categories(models.Model):
-    name = models.CharField(max_length=150, unique=True, verbose_name='Название')
-    slug = models.SlugField(max_length=200, unique=True, blank=True, null=True, verbose_name= 'url')
+    name = models.CharField(max_length=150, unique=True, verbose_name="Название")
+    slug = models.SlugField(
+        max_length=200, unique=True, blank=True, null=True, verbose_name="url"
+    )
 
     class Meta:
-        db_table = 'category'
-        verbose_name = 'Категорию'
-        verbose_name_plural = 'Категории'
+        db_table = "category"
+        verbose_name = "Категорию"
+        verbose_name_plural = "Категории"
 
     def __str__(self) -> str:
         return self.name
 
-class Products(models.Model):
-    name = models.CharField(max_length=150, unique=True, verbose_name='Название')
-    slug = models.SlugField(max_length=200, unique=True, blank=True, null=True, verbose_name= 'url')
-    description = models.TextField(blank=True, null=True, verbose_name='Описание')
-    image = models.ImageField(upload_to='goods_images', blank=True, null=True, verbose_name='Изображение')
-    price = models.DecimalField(default=0.00, max_digits=7, decimal_places=2, verbose_name= 'Цена')
-    quantity = models.PositiveIntegerField(default=0, verbose_name='Количество')
-    category = models.ForeignKey(to=Categories, on_delete=models.PROTECT )
 
+class Products(models.Model):
+    name = models.CharField(max_length=150, unique=True, verbose_name="Название")
+    slug = models.SlugField(
+        max_length=200, unique=True, blank=True, null=True, verbose_name="url"
+    )
+    description = models.TextField(blank=True, null=True, verbose_name="Описание")
+    image = models.ImageField(
+        upload_to="goods_images", blank=True, null=True, verbose_name="Изображение"
+    )
+    price = models.DecimalField(
+        default=0.00, max_digits=7, decimal_places=2, verbose_name="Цена"
+    )
+    quantity = models.PositiveIntegerField(default=0, verbose_name="Количество")
+    category = models.ForeignKey(to=Categories, on_delete=models.PROTECT)
 
     class Meta:
-        db_table = 'product'
-        verbose_name = 'Продукт'
-        verbose_name_plural = 'Продукты'
+        db_table = "product"
+        verbose_name = "Продукт"
+        verbose_name_plural = "Продукты"
         ordering = ("id",)
 
     def __str__(self) -> str:
-        return f'{self.name} Количество - {self.quantity}'
-    
+        return f"{self.name} Количество - {self.quantity}"
+
     def display_id(self):
-        
+
         return f"{self.id:05}"
 
+    def sell_price(self):
+        return self.price
--- store/users/views.py
+++ store/users/views.py
@@ -8,77 +8,81 @@ from django.contrib.auth.decorators import login_required
 
 # Create your views here.
 
+
 def login(request):
-    if request.method == 'POST':
+    if request.method == "POST":
         form = UserLoginForm(data=request.POST)
         if form.is_valid():
-            username = request.POST['username']
-            password = request.POST['password']
+            username = request.POST["username"]
+            password = request.POST["password"]
             user = auth.authenticate(username=username, password=password)
             if user:
                 auth.login(request, user)
                 messages.success(request, f"{username}, Вы вошли в аккаунт")
 
-                if request.POST.get(('next', None)):
-                    return HttpResponseRedirect(request.POST.get('next'))
-                
-
-                return HttpResponseRedirect(reverse('main:index'))
+                redirect_page = request.POST.get("next", None)
+                if redirect_page and redirect_page != reverse("user:logout"):
+                    return HttpResponseRedirect(request.POST.get("next"))
+                return HttpResponseRedirect(reverse("main:index"))
     else:
         form = UserLoginForm()
 
     context = {
-        'title': 'Roga&Kopyta - Авторизация',
-        'form': form ,
-        
+        "title": "Roga&Kopyta - Авторизация",
+        "form": form,
     }
-    return render(request, 'users/login.html', context)
+    return render(request, "users/login.html", context)
+
 
 def registration(request):
-    if request.method == 'POST':
+    if request.method == "POST":
         form = UserRegistrationForm(data=request.POST)
         if form.is_valid():
             form.save()
             user = form.instance
-            auth.login(request,user)
-            messages.success(request, f"{user.username}, Вы успешно зарегестрировались и вошли в аккаунт")     
-            return HttpResponseRedirect(reverse('main:index'))
+            auth.login(request, user)
+            messages.success(
+                request,
+                f"{user.username}, Вы успешно зарегестрировались и вошли в аккаунт",
+            )
+            return HttpResponseRedirect(reverse("main:index"))
     else:
         form = UserRegistrationForm()
 
     context = {
-        'title': 'Roga&Kopyta - Регистрация',
-        'form': form ,
-        
+        "title": "Roga&Kopyta - Регистрация",
+        "form": form,
     }
-    return render(request, 'users/registration.html', context)
+    return render(request, "users/registration.html", context)
 
 
 @login_required
 def profile(request):
-    if request.method == 'POST':
-        form = ProfileForm(data=request.POST, instance=request.user,files=request.FILES)
+    if request.method == "POST":
+        form = ProfileForm(
+            data=request.POST, instance=request.user, files=request.FILES
+        )
         if form.is_valid():
             form.save()
-            messages.success(request, "Профиль успешно обновлен")   
-            return HttpResponseRedirect(reverse('user:profile'))
+            messages.success(request, "Профиль успешно обновлен")
+            return HttpResponseRedirect(reverse("user:profile"))
     else:
         form = UserRegistrationForm(instance=request.user)
 
-    
     context = {
-        'title': 'Roga&Kopyta - Кабинет',
-        'form': form ,
-        
+        "title": "Roga&Kopyta - Кабинет",
+        "form": form,
     }
-    return render(request, 'users/profile.html', context)
+    return render(request, "users/profile.html", context)
+
 
 def users_cart(request):
-    
-    return render(request, 'users/users_cart.html')
+
+    return render(request, "users/users_cart.html")
+
 
 @login_required
 def logout(request):
     auth.logout(request)
     messages.success(request, f"{request.user.username}, Вы вышли из аккаунта")
-    return redirect(reverse('main:index'))
+    return redirect(reverse("main:index"))
